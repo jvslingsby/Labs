010f455347c5b35d91f9c3e621efa1fcfa36104f
jvslingsby <jvslingsby@gmail.com>
2024-04-21 16:26:27 -0700

    Lab 4 Branch: Updating typo on the branch Lab4

--- modules/compute/variables.tf
+++ modules/compute/variables.tf
@@ -7,13 +7,13 @@ variable "region" {
 
 variable "ssh_key_public" {
   type    = string
-  #Replace this with the location of you public key .pub
+  #Replace this with the location of your public key .pub
   default = "C:\\Users\\JV Slingsby\\.ssh\\id_rsa.pub"
 }
 
 variable "ssh_key_private" {
   type    = string
-  #Replace this with the location of you private key
+  #Replace this with the location of your private key
   default = "C:\\Users\\JV Slingsby\\.ssh\\id_rsa"
 }
 
