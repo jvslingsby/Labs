1658304e75c33e987e4ff9dbd98a9fc8d6190b23
jvslingsby <jvslingsby@gmail.com>
2024-04-21 17:22:44 -0700

    Lab5: commits

--- outputs.tf
+++ outputs.tf
@@ -1,5 +1,5 @@
 #-----outputs.tf-----
 #====================
-output "Sever-Public-IP" {
-  value = "${module.compute.server_ip}"
+output "Jenkins-Public-URL" {
+  value = "http://${module.compute.server_ip}:8080"
 }
